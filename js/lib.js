8b9ce116f69c50b8f908794c96af082fdcb7fa70
Zin Moe <dev.zinmoe@gmail.com>
2024-05-14 11:35:38 +0630

    modify image award caption css

--- js/lib.js
+++ js/lib.js
@@ -5,23 +5,23 @@
    * ===========================
    */
 
-  var navbar = document.querySelector(".nav_wrapper");
-  var logoContainer = document.querySelector(".logo_container");
+  var navbar = document.querySelector('.nav_wrapper');
+  var logoContainer = document.querySelector('.logo_container');
   var prevY = 0;
   var startPixel = 30; // to start show and hide
   if (navbar) {
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
       var scrolledPixels = document.documentElement.scrollTop;
       if (scrolledPixels < prevY && scrolledPixels > startPixel) {
-        navbar.style.top = "0%";
-        navbar.style.paddingTop = "0";
-        logoContainer.classList.add("logo_container--small");
+        navbar.style.top = '0%';
+        navbar.style.paddingTop = '0';
+        logoContainer.classList.add('logo_container--small');
       } else if (scrolledPixels < prevY) {
-        navbar.style.top = "0%";
-        navbar.style.paddingTop = "25px";
-        logoContainer.classList.remove("logo_container--small");
+        navbar.style.top = '0%';
+        navbar.style.paddingTop = '25px';
+        logoContainer.classList.remove('logo_container--small');
       } else if (scrolledPixels > startPixel) {
-        navbar.style.top = "-100%";
+        navbar.style.top = '-100%';
       }
       prevY = scrolledPixels;
     });
@@ -33,33 +33,33 @@
    * Search Bar Toggle
    * ===========================
    */
-  var searchIcon = document.querySelector(".links_container .search_button");
-  var searchBar = document.querySelector(".croucher_search_container");
+  var searchIcon = document.querySelector('.links_container .search_button');
+  var searchBar = document.querySelector('.croucher_search_container');
   var searchCrossIcon = document.querySelector(
-    ".croucher_search_container .cross_icon"
+    '.croucher_search_container .cross_icon'
   );
   if (searchIcon && searchBar && searchCrossIcon) {
     var scrollToTopWithScroll = function (time = 350) {
       setTimeout(function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
+        window.scrollTo({ top: 0, behavior: 'smooth' });
       }, time);
     };
 
-    searchIcon.addEventListener("click", function () {
+    searchIcon.addEventListener('click', function () {
       var searchBarOpen = searchBar.classList.contains(
-        "croucher_search_container--open"
+        'croucher_search_container--open'
       );
       scrollToTopWithScroll(searchBarOpen ? 0 : 200);
       if (searchBarOpen) return;
-      searchBar.classList.add("croucher_search_container--open");
+      searchBar.classList.add('croucher_search_container--open');
     });
 
-    searchCrossIcon.addEventListener("click", function () {
+    searchCrossIcon.addEventListener('click', function () {
       var searchBarOpen = searchBar.classList.contains(
-        "croucher_search_container--open"
+        'croucher_search_container--open'
       );
       if (!searchBarOpen) return;
-      searchBar.classList.remove("croucher_search_container--open");
+      searchBar.classList.remove('croucher_search_container--open');
     });
   }
 
@@ -68,24 +68,24 @@
    * I-Format Photo Text
    * ===========================
    */
-  var iFormats = document.querySelectorAll(".i-format");
+  var iFormats = document.querySelectorAll('.active-icon');
   if (iFormats && iFormats.length > 0) {
     iFormats.forEach(function (iIcon) {
-      iIcon.addEventListener("click", function () {
-        var textContainer = this.parentElement.querySelector(".i-format-text");
-        if (textContainer && !textContainer.classList.contains("__open")) {
-          textContainer.classList.add("__open");
+      iIcon.addEventListener('click', function () {
+        var parent = this.parentElement;
+        if (parent && !parent.classList.contains('active')) {
+          parent.classList.add('active');
         }
       });
     });
   }
-  var iCloseIcons = document.querySelectorAll(".i-format-text .close-icon");
+  var iCloseIcons = document.querySelectorAll('.close-icon');
   if (iCloseIcons && iCloseIcons.length > 0) {
     iCloseIcons.forEach(function (closeIcon) {
-      closeIcon.addEventListener("click", function () {
-        var textContainer = this.parentElement;
-        if (textContainer && textContainer.classList.contains("__open")) {
-          textContainer.classList.remove("__open");
+      closeIcon.addEventListener('click', function () {
+        var parent = this.parentElement;
+        if (parent && parent.classList.contains('active')) {
+          parent.classList.remove('active');
         }
       });
     });
