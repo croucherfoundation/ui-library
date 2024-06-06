3c66bec620d1d23081bc6e6cff63bcd1381db620
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-06-06 14:28:23 +0630

    add throttle and change top

--- js/lib.js
+++ js/lib.js
@@ -1,68 +1,36 @@
 (function () {
   /**
    * ===========================
-   * Navbar Scroll Show Hide Start
-   * ===========================
-   */
-
-  // var navbar = document.querySelector('.nav_wrapper');
-  // var logoContainer = document.querySelector('.logo_container');
-  // var prevY = 0;
-  // var startPixel = 30; // to start show and hide
-  // if (navbar) {
-  //   window.addEventListener('scroll', () => {
-  //     var scrolledPixels = document.documentElement.scrollTop;
-  //     if (scrolledPixels < prevY && scrolledPixels > startPixel) {
-  //       // navbar.style.top = '0%';
-  //       navbar.style.paddingTop = '0';
-  //       logoContainer.classList.add('logo_container--small');
-  //       navbar.classList.remove("hide");
-  //     } else if (scrolledPixels < prevY) {
-  //       // navbar.style.top = '0%';
-  //       navbar.style.paddingTop = '25px';
-  //       logoContainer.classList.remove('logo_container--small');
-  //       navbar.classList.remove("hide");
-  //     } else if (scrolledPixels > startPixel) {
-  //       // navbar.style.top = '-100%';
-  //       navbar.classList.add("hide");
-  //     }
-  //     prevY = scrolledPixels;
-  //   });
-  // }
-  /* ---- Navbar Scroll Finished ---- */
-
-  /**
-   * ===========================
    * Search Bar Toggle
    * ===========================
    */
-  var searchIcon = document.querySelector('.links_container .search_button');
-  var searchBar = document.querySelector('.croucher_search_container');
+  var searchIcon = document.querySelector(".links_container .search_button");
+  var searchBar = document.querySelector(".croucher_search_container");
   var searchCrossIcon = document.querySelector(
-    '.croucher_search_container .cross_icon'
+    ".croucher_search_container .cross_icon"
   );
   if (searchIcon && searchBar && searchCrossIcon) {
     var scrollToTopWithScroll = function (time = 350) {
       setTimeout(function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+        window.scrollTo({ top: 0, behavior: "smooth" });
       }, time);
     };
 
-    searchIcon.addEventListener('click', function () {
+    searchIcon.addEventListener("click", function () {
       var searchBarOpen = searchBar.classList.contains(
-        'croucher_search_container--open'
+        "croucher_search_container--open"
       );
       scrollToTopWithScroll(searchBarOpen ? 0 : 200);
       if (searchBarOpen) return;
-      searchBar.classList.add('croucher_search_container--open');
+      searchBar.classList.add("croucher_search_container--open");
     });
 
-    searchCrossIcon.addEventListener('click', function () {
+    searchCrossIcon.addEventListener("click", function () {
       var searchBarOpen = searchBar.classList.contains(
-        'croucher_search_container--open'
+        "croucher_search_container--open"
       );
       if (!searchBarOpen) return;
-      searchBar.classList.remove('croucher_search_container--open');
+      searchBar.classList.remove("croucher_search_container--open");
     });
   }
 
@@ -71,26 +39,26 @@
    * I-Format Photo Text
    * ===========================
    */
-  var iFormats = document.querySelectorAll('.active-icon');
+  var iFormats = document.querySelectorAll(".active-icon");
   if (iFormats && iFormats.length > 0) {
     iFormats.forEach(function (iIcon) {
-      iIcon.addEventListener('click', function () {
+      iIcon.addEventListener("click", function () {
         var parent = this.parentElement;
-        if (parent && !parent.classList.contains('active')) {
-          parent.classList.add('active');
+        if (parent && !parent.classList.contains("active")) {
+          parent.classList.add("active");
         }
       });
     });
   }
-  var iCloseIcons = document.querySelectorAll('.close-icon');
+  var iCloseIcons = document.querySelectorAll(".close-icon");
   if (iCloseIcons && iCloseIcons.length > 0) {
     iCloseIcons.forEach(function (closeIcon) {
-      closeIcon.addEventListener('click', function () {
+      closeIcon.addEventListener("click", function () {
         var parent = this.parentElement;
-        if (parent && parent.classList.contains('active')) {
-          parent.classList.remove('active');
+        if (parent && parent.classList.contains("active")) {
+          parent.classList.remove("active");
         }
-        
+
         var grandParent = parent.parentElement;
         if (grandParent && grandParent.classList.contains("active")) {
           grandParent.classList.remove("active");
@@ -99,36 +67,35 @@
     });
   }
 
-
   /**
    * ===========================
    * Main and Sub Navbar Container
    * ===========================
+   * need to add lodash cdn link
    */
 
-  var navbar = document.querySelector(".main_sub_navbar_container");
-  var logoContainer = document.querySelector(".logo_container");
-  var mainNavContainer = document.querySelector(".nav_wrapper");
-  var prevY = 0;
-  var startPixel = 120; // to start show and hide
-  if (navbar) {
-    window.addEventListener("scroll", () => {
+  window.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.querySelector(".main_sub_navbar_container");
+    var mainNavContainer = document.querySelector(".nav_wrapper");
+    var prevY = 0;
+    var startPixel = 120; // to start show and hide
+    function handleScroll() {
       var scrolledPixels = document.documentElement.scrollTop;
       var { height } = mainNavContainer.getBoundingClientRect();
-      var gap = 0;
-      if (scrolledPixels < prevY && scrolledPixels > startPixel) {
-        mainNavContainer.classList.add("no_padding");
-        navbar.style.transform = `translate(-50%, 0)`;
-      } else if(scrolledPixels < prevY) {
-        // scrolling up -> show
-        navbar.style.transform = `translate(-50%, 0)`;
-        mainNavContainer.classList.remove("no_padding");
-      } else {
-        // scrolling down -> hide
-        navbar.style.transform = `translate(-50%, -${gap + height}px)`;
+      if (scrolledPixels < prevY) {
+        navbar.style.top = "0px";
+        if (scrolledPixels > startPixel) {
+          mainNavContainer.classList.add("no_padding");
+        } else {
+          mainNavContainer.classList.remove("no_padding");
+        }
+      } else if (scrolledPixels > 0) {
+        navbar.style.top = `-${height}px`;
       }
-      prevY = scrolledPixels;
-    });
-  }
-  
+      prevY = scrolledPixels <= 0 ? 0 : scrolledPixels;
+    }
+    if (navbar) {
+      window.addEventListener("scroll", _.throttle(handleScroll, 250));
+    }
+  });
 })();
